52fb831d74fc0705a78f36a4b19b5df417da9a88
SaiC0D3 <matias2004.morales@gmail.com>
2025-08-28 01:16:48 -0400

    Documentación  orden de funciones :)

--- combinatoria.c
+++ combinatoria.c
@@ -20,44 +20,21 @@
 #define THIRD     5
 #define THIRD_OP  6
 
+/*
+*
+*
+*/
+
 void Usage(char const *);
 
-// Funcion de factorial recursiva para el valor n
-unsigned long int FactorialRec(unsigned long int num){
+// Funcion de factorial recursiva para el valor num
+unsigned long int FactorialRec(unsigned long int num);
 
-    if(num == 0 || num == 1){
-        return 1;
-    }
-        
-    return num * FactorialRec(num - 1);
-}
-
-unsigned long int Factorial(unsigned long int num) {
-
-    unsigned int i;
-    unsigned long int prod = 1;
-
-    if (num == 0) {
-        return 1;
-    }
-        
-    for (i = 1; i <= num; i = i + 1) {
-        prod = prod * i;
-    }
-
-    return prod;
-}
+// Funcion de factorial iterativa para el valor num
+unsigned long int Factorial(unsigned long int num);
 
 // Primer metodo: C(n,k) = n! / (n-k)! * k!
-unsigned long int FirstMethod(unsigned long int n, unsigned long int k, unsigned char op){
-
-    if (op = FIRST) {
-        return (Factorial(n)) / (Factorial(n - k) * (Factorial(k)));
-    }
-    else if (op == FIRST_REC) {
-        return (FactorialRec(n)) / (FactorialRec(n - k) * (FactorialRec(k)));
-    }
-}
+unsigned long int FirstMethod(unsigned long int n, unsigned long int k, unsigned char op);
 
 // Segundo metodo: C(n,k) = C(n-1,k-1) + C(n-1,k)
 /*
@@ -72,58 +49,17 @@ unsigned long int FirstMethod(unsigned long int n, unsigned long int k, unsigned
         Multiplicidad de i = 1 hasta k + 1 de (n - i) / (k - 1)!
 */
 
-unsigned long int SecondMethod(unsigned long int n, unsigned long int k) {
+unsigned long int SecondMethod(unsigned long int n, unsigned long int k);
 
-    unsigned int i;
-    unsigned long int result;
+unsigned long int SecondMethodOptimized(unsigned long int n, unsigned long int k);
 
-    if(k == 0 || k == n) {
-        return 1;
-    }
+// Tercer metodo: multiplicatoria_{i=1}^{k} (n-i+1/i)
+unsigned long int ThirdMethod(unsigned long int n, unsigned long int k, unsigned char op);
 
-    return (SecondMethod(n - 1, k - 1) + SecondMethod(n - 1, k));
-}
-
-unsigned long int SecondMethodOptimized(unsigned long int n, unsigned long int k) {
-
-    unsigned int i;
-    unsigned long int multiplicity = 1, result;
-
-    // Resuelve la multiplicidad (simplificacion)
-    for (i = 1; i <= k - 1; i = i + 1) {
-        multiplicity = (n - i) * multiplicity;
-    }
-        
-    result = multiplicity / Factorial(k - 1);
-
-    return (result + Factorial(n - 1) / (Factorial(k) * Factorial(n - 1 - k)));
-}
-
-unsigned long int ThirdMethod(unsigned long int n, unsigned long int k, unsigned char op) {
-
-    unsigned int i;
-    unsigned long int result = 1;
-
-    if (k > n) {
-        return 0;
-    }
-    else if(k == 0 || k == n) {
-        return 1;
-    }
-
-    // Usamos simetría C(n,k) = C(n,n-k)
-    if (op == THIRD_OP) {
-        if (k > n - k) {
-            k = n - k;
-        }
-    }      
-
-    for (i = 1; i <= k; i = i + 1) {
-        result = result * ((n - i + 1) / i);
-    }
-
-    return result;
-}
+/*
+*
+*
+*/
 
 int main(int argc, char const **argv) { // 'argc' contiene la cantidad de parámetros de entrada, 'argv' contiene cada parámetro ingresado (cadena de cadenas, doble vector)
     
@@ -190,7 +126,100 @@ int main(int argc, char const **argv) { // 'argc' contiene la cantidad de parám
     return 0;
 }
 
+/*
+*
+*
+*/
+
 void Usage(char const *msg) {
     printf("\nUsage: %s -o n k", msg);
     printf("\n\no in {0,1,2,3}\n\n");
+}
+
+unsigned long int FactorialRec(unsigned long int num) {
+
+    if(num == 0 || num == 1){
+        return 1;
+    }
+        
+    return num * FactorialRec(num - 1);
+}
+
+unsigned long int Factorial(unsigned long int num) {
+
+    unsigned int i;
+    unsigned long int prod = 1;
+
+    if (num == 0) {
+        return 1;
+    }
+        
+    for (i = 1; i <= num; i = i + 1) {
+        prod = prod * i;
+    }
+
+    return prod;
+}
+
+unsigned long int FirstMethod(unsigned long int n, unsigned long int k, unsigned char op) {
+
+    if (op = FIRST) {
+        return (Factorial(n)) / (Factorial(n - k) * (Factorial(k)));
+    }
+    else if (op == FIRST_REC) {
+        return (FactorialRec(n)) / (FactorialRec(n - k) * (FactorialRec(k)));
+    }
+}
+
+unsigned long int SecondMethod(unsigned long int n, unsigned long int k) {
+
+    unsigned int i;
+    unsigned long int result;
+
+    if(k == 0 || k == n) {
+        return 1;
+    }
+
+    return (SecondMethod(n - 1, k - 1) + SecondMethod(n - 1, k));
+}
+
+unsigned long int SecondMethodOptimized(unsigned long int n, unsigned long int k) {
+
+    unsigned int i;
+    unsigned long int multiplicity = 1, result;
+
+    // Resuelve la multiplicidad (simplificacion)
+    for (i = 1; i <= k - 1; i = i + 1) {
+        multiplicity = (n - i) * multiplicity;
+    }
+        
+    result = multiplicity / Factorial(k - 1);
+
+    return (result + Factorial(n - 1) / (Factorial(k) * Factorial(n - 1 - k)));
+}
+
+unsigned long int ThirdMethod(unsigned long int n, unsigned long int k, unsigned char op) {
+
+    unsigned int i;
+    unsigned long int result = 1;
+
+    if (k > n) {
+        return 0;
+    }
+    else if(k == 0 || k == n) {
+        return 1;
+    }
+
+    // Usamos simetría C(n,k) = C(n,n-k)
+    if (op == THIRD_OP) {
+        if (k > n - k) {
+            k = n - k;
+        }
+    }      
+
+    for (i = 1; i <= k; i = i + 1) {
+        result = result * ((n - i + 1) / i);
+    }
+
+    return result;
 }
